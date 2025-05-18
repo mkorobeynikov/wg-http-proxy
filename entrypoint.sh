57254b81e7701c88e7bed62d9f21287b83a48dea
Maksim Korobeynikov <maksim.korobeynikov@gazprombank.ru>
2025-05-18 20:31:34 +0300

    Fix: not worked on start

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,8 @@ wg-quick up /etc/wireguard/wg0.conf
 
 # 2. Запускаем Tinyproxy в режимеForeground (не daemon, чтобы держать процесс активным)
 exec tinyproxy -c /etc/tinyproxy/tinyproxy.conf -d
+
+# 3. Какой-то грязный хак, потому что сразу не хочет работать
+# TODO: fixit
+wg-quick down /etc/wireguard/wg0.conf
+wg-quick up /etc/wireguard/wg0.conf
